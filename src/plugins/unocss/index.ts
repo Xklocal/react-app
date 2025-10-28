377f9729cba03c157512918bf8ff6c1e45afde1f
admin <2693644866@qq.com>
2025-10-28 11:52:02 +0800

    配置unocss

---
+++ src/plugins/unocss/index.ts
@@ -0,0 +1,10 @@
+// normalize.css
+import '@unocss/reset/normalize.css';
+// reset.css by Eric Meyer https://meyerweb.com/eric/tools/css/reset/index.html
+import '@unocss/reset/eric-meyer.css';
+// preflights from tailwind
+import '@unocss/reset/tailwind.css';
+// https://github.com/unocss/unocss/blob/main/packages/reset/tailwind-compat.md
+import '@unocss/reset/tailwind-compat.css';
+
+import 'uno.css';
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path"; // Node 内置模块，用于处理路径
+import UnoCSS from "unocss/vite"; // 引入 UnoCSS 插件
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
+    UnoCSS(),
   ],
-})
+  resolve: {
+    // 配置路径别名 用于@引用
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // 让 @ 指向 src 目录
+    },
+  },
+});
